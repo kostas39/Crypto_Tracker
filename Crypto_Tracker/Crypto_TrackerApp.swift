1b3531808c38f81cb0f873d9708cd9c5114bd950
konstantinos koliolios <kolioliosk@gmail.com>
2024-04-25 15:31:58 +0100

    Initial Commit

---
+++ Crypto_Tracker/Crypto_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_TrackerApp.swift
+//  Crypto_Tracker
+//
+//  Created by Kostas Koliolios on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
