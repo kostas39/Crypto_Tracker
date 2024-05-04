9f03d7ee85fa1d2dc54866dacd1958bb3ddf9bd9
konstantinos koliolios <kolioliosk@gmail.com>
2024-05-04 12:55:49 +0300

    setting up the top moving coins

--- Crypto_Tracker/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto_Tracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 class HomeViewModel: ObservableObject {
-    @Published var coins = [Coin] ()
-    
+    @Published var coins = [Coin]()
+    @Published var topMovingCoins = [Coin]()
+
     init() {
         fetchCoinData()
     }
@@ -43,5 +44,10 @@ class HomeViewModel: ObservableObject {
             }
             
         }.resume()
+    }
+    
+    func configureTopMovingCoins() {
+        let topMovers = coins.sorted(by: { $0.priceChangePercentage24H > $1.priceChangePercentage24H })
+        self.topMovingCoins = Array(topMovers.prefix(5))
     }
 }
